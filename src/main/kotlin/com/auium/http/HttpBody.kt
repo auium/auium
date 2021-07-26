62086189a8bf75b3348fda65416b87f311bc03bc
zhongyu <zhongyu.vip@gmail.com>
2021-07-26 09:19:18 +0800

    add docs

--- src/main/kotlin/com/auium/http/HttpBody.kt
+++ src/main/kotlin/com/auium/http/HttpBody.kt
@@ -8,17 +8,32 @@ import okhttp3.RequestBody.Companion.toRequestBody
 
 private val jsonMediaType = "application/json; charset=utf-8".toMediaType()
 
+/**
+ * Build OkHttp FormBody request
+ * @param params Map Params
+ * @return Request.Builder
+ */
 fun Request.Builder.formBody(params: Map<String, Any> = mapOf()): Request.Builder {
     val body = FormBody.Builder()
     params.forEach { (key, value) -> body.add(key, "$value") }
     return post(body.build())
 }
 
+/**
+ * Build OkHttp JSON request
+ * @param json JSON String
+ * @return Request.Builder
+ */
 fun Request.Builder.jsonBody(json: String): Request.Builder {
     val body = json.toRequestBody(jsonMediaType)
     return post(body)
 }
 
+/**
+ * Build OkHttp JSON request
+ * @param obj Object
+ * @return Request.Builder
+ */
 fun Request.Builder.jsonBody(obj: Any): Request.Builder {
     val body = obj.jsonString().toRequestBody(jsonMediaType)
     return post(body)
--- src/main/kotlin/com/auium/http/HttpResult.kt
+++ src/main/kotlin/com/auium/http/HttpResult.kt
@@ -2,5 +2,4 @@ package com.auium.http
 
 data class HttpResult(val code: Int,val headers: Map<String, String>, val body: String) {
 
-
 }
