cb62f4d13d37418c7e23eba0dac6e8460805df9d
zhongyu <zhongyu.vip@gmail.com>
2021-08-08 11:45:17 +0800

    add doc

--- src/main/kotlin/com/auium/element/IElement.kt
+++ src/main/kotlin/com/auium/element/IElement.kt
@@ -6,20 +6,45 @@ import java.awt.Rectangle
 
 interface IElement {
 
+    /**
+     * 通过坐标点击元素
+     */
     fun tap()
 
+    /**
+     * 获取元素文本内容
+     */
     fun text(): String
 
+    /**
+     * 获取文本元素大小
+     */
     fun size(): Dimension
 
+    /**
+     * 获取元素的矩形信息
+     */
     fun rect(): Rectangle
 
+    /**
+     * 获取元素的位置信息
+     */
     fun location(): Point
 
+    /**
+     * 获取元素的中心位置
+     */
     fun center(): Point
 
+    /**
+     * 清空元素内容
+     */
     fun clear()
 
+    /**
+     * 输入内容
+     * @param keys 需要输入的内容
+     */
     fun sendKeys(keys: String)
 
 }
--- src/main/kotlin/com/auium/element/Selector.kt
+++ src/main/kotlin/com/auium/element/Selector.kt
@@ -1,6 +1,9 @@
 package com.auium.element
 
-
+/**
+ * 定位选择器
+ * @param using 定位方式
+ */
 sealed class Selector(val using: String) {
 
     data class Id(val id: String) : Selector("id")
@@ -9,6 +12,6 @@ sealed class Selector(val using: String) {
     data class XPath(val xpath: String) : Selector("xpath")
     data class Predicate(val predicate: String) : Selector("predicate string")
     data class ClassChain(val classChain: String) : Selector("class chain")
-    data class NameContains(val nameContains: String): Selector("class chain")
+    data class NameContains(val nameContains: String) : Selector("class chain")
 
 }
