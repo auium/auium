a9020fa7759589f13bea7d3e0dd8d41d903488a6
zhongyu <zhongyu.vip@gmail.com>
2021-08-11 14:10:53 +0800

    add element type

--- src/main/kotlin/com/auium/element/ElementType.kt
+++ src/main/kotlin/com/auium/element/ElementType.kt
@@ -5,9 +5,12 @@ enum class ElementType(val completeType: String) {
     Any("XCUIElementTypeAny"),
     Cell("XCUIElementTypeCell"),
     Text("XCUIElementTypeStaticText"),
+    Link("XCUIElementTypeLink"),
     Input("XCUIElementTypeTextField"),
     Button("XCUIElementTypeButton"),
     Image("XCUIElementTypeImage"),
     Other("XCUIElementTypeOther"),
+    Switch("XCUIElementTypeSwitch"),
+    WebView("XCUIElementTypeWebView"),
 
 }
